01c97c03b184349c852048f0bd5d44db8c0cf9d4
sgJung <alow9942@daum.net>
2022-11-25 10:16:00 +0900

    11/25 10:15 JSG

--- Assets/Scripts/NPC/NPCTrigger.cs
+++ Assets/Scripts/NPC/NPCTrigger.cs
@@ -12,14 +12,29 @@ public class NPCTrigger : MonoBehaviour
     public GameObject player;
     public GameObject parent;
 
+    // NPC 이름 및 기본 대화
     public string npcName;
     public string[] npcTalk;
+
+    public GameObject questBtn;
+    public TMP_Text questBtnText;
+    // NPC 퀘스트 대화(한 배열에 한 퀘스트 씩, 첫 문장은 퀘스트 이름으로 쓸 것)
+    public string[] npcQuestTalk;
+    int npcQuestIndex = 0;
+    
+    [HideInInspector]
+    public int currentQuestIndex = 0;
+
+    Dictionary<int, string[]> npcQuestList;
+    string questName;
+
     string curNpcTalk;
     int npcTalkIndex = 0;
     bool isTriggerInPlayer = false;
 
     void Awake()
     {
+        SetQuestList();
     }
 
     void Update()
@@ -28,7 +43,20 @@ public class NPCTrigger : MonoBehaviour
         {
             RotationNPCtoPlayer();
             FirstTalkWithPlayer();
-            TalkSkipOrNext();
+            //TalkSkipOrNext();
+        }
+    }
+
+    void SetQuestList()
+    {
+        npcQuestList = new Dictionary<int, string[]>();
+
+        foreach(string q in npcQuestTalk )
+        {
+            string[] quest = q.Split("//");
+            npcQuestList.Add(npcQuestIndex, quest);
+
+            npcQuestIndex++;
         }
     }
 
@@ -74,12 +102,7 @@ public class NPCTrigger : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.F) && !PlayerMove.Instance.isF && !TalkManager.Instance.isTalk)
         {
-            PlayerMove.Instance.isF = true;
-            TalkManager.Instance.NPCChatEnter(npcName);
-
-            curNpcTalk = npcTalk[npcTalkIndex];
-            TalkManager.Instance.NPCChat(curNpcTalk);
-            npcTalkIndex++;
+            TalkManager.Instance.FirstTalkWithPlayer(npcName, npcTalk);
         }
     }
 
@@ -92,11 +115,22 @@ public class NPCTrigger : MonoBehaviour
         }
         else if (Input.GetKeyDown(KeyCode.Space) && PlayerMove.Instance.isF && TalkManager.Instance.isTalk && !TalkManager.Instance.isCor) // NPC의 대화 한 문장이 출력 된 후 다음 문장으로 넘어가기 위함
         {
-            if (npcTalkIndex >= npcTalk.Length)
+            if (npcTalkIndex >= npcTalk.Length) // 마지막 대화라면
             {
-                print("Chat End");
-                TalkManager.Instance.NPCChatExit();
-                npcTalkIndex = 0;
+                if(currentQuestIndex < npcQuestIndex && !PlayerMove.Instance.isQuesting) // 수행할 수 있는 퀘스트가 존재하고 현재 플레이어가 다른 퀘스트를 하고 있지 않은 경우
+                {
+                    questName = npcQuestList[currentQuestIndex][0];
+                    questBtn.SetActive(true);
+                    questBtnText.text = questName;
+
+                    QuestManager.Instance.QuestEnter(questName, npcQuestList[currentQuestIndex], this);
+                }
+                else // 퀘스트가 없거나 다른 퀘스트를 수행중인 경우
+                {
+                    print("Chat End");
+                    TalkManager.Instance.NPCChatExit();
+                    npcTalkIndex = 0;
+                }
                 return;
             }
             print("next Chat");
@@ -105,6 +139,11 @@ public class NPCTrigger : MonoBehaviour
             TalkManager.Instance.NPCChat(curNpcTalk);
             npcTalkIndex++;
         }
+    }
+
+    public void OnClickQuestBtn()
+    {
+        print("hi Im quest");
     }
 
 }
--- Assets/Scripts/TalkManager.cs
+++ Assets/Scripts/TalkManager.cs
@@ -22,12 +22,20 @@ public class TalkManager : MonoBehaviour
     public GameObject playerUI;
     public GameObject questUI;
     public GameObject dropDownImg;
+    public GameObject questBtn;
     public TMP_Text npcTalkText;
     public TMP_Text nameText;
+    public TMP_Text questBtnText;
 
     public bool isTalk = false;
     [HideInInspector]
     public bool isCor = false;
+
+    string npcName;
+    string[] npcTalk;
+
+    string curNpcTalk;
+    int npcTalkIndex = 0;
 
     void Awake()
     {
@@ -45,7 +53,10 @@ public class TalkManager : MonoBehaviour
 
     void Update()
     {
-
+        if (isTalk)
+        {
+            TalkSkipOrNext();
+        }
     }
 
     public void NPCChatEnter(string npcName)
@@ -89,6 +100,53 @@ public class TalkManager : MonoBehaviour
         dropDownImg.SetActive(true);
 
         isCor = false;
+    }
+
+    public void FirstTalkWithPlayer(string npcName_, string[] npcTalk_)
+    {
+        npcName = npcName_;
+        npcTalk = npcTalk_;
+        PlayerMove.Instance.isF = true;
+        NPCChatEnter(npcName);
+
+        curNpcTalk = npcTalk[npcTalkIndex];
+        NPCChat(curNpcTalk);
+        npcTalkIndex++;
+    }
+
+    void TalkSkipOrNext()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isCor) // 대화 스킵
+        {
+            QuickStopChat(curNpcTalk);
+            print("Chat Skip");
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && PlayerMove.Instance.isF && isTalk && !isCor) // NPC의 대화 한 문장이 출력 된 후 다음 문장으로 넘어가기 위함
+        {
+            if (npcTalkIndex >= npcTalk.Length) // 마지막 대화라면
+            {
+               /* if (currentQuestIndex < npcQuestIndex && !PlayerMove.Instance.isQuesting) // 수행할 수 있는 퀘스트가 존재하고 현재 플레이어가 다른 퀘스트를 하고 있지 않은 경우
+                {
+                    questName = npcQuestList[currentQuestIndex][0];
+                    questBtn.SetActive(true);
+                    questBtnText.text = questName;
+
+                    QuestManager.Instance.QuestEnter(questName, npcQuestList[currentQuestIndex], this);
+                }
+                else // 퀘스트가 없거나 다른 퀘스트를 수행중인 경우
+                {
+                    print("Chat End");
+                    NPCChatExit();
+                    npcTalkIndex = 0;
+                }*/
+                return;
+            }
+            print("next Chat");
+
+            curNpcTalk = npcTalk[npcTalkIndex];
+            NPCChat(curNpcTalk);
+            npcTalkIndex++;
+        }
     }
 
     public void QuickStopChat(string npcTalk)
