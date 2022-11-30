6454bf84652c7294c9d227179acc792af7ccca17
agfalcon <agfalcon12@gmail.com>
2022-11-30 17:05:45 +0900

    game setting & option complete

--- Assets/Scripts/UI/ScreenResolution.cs
+++ Assets/Scripts/UI/ScreenResolution.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScreenResolution : MonoBehaviour
 {
+    FullScreenMode screenMode;
     public GameObject resolutionDropDownObject;
-    Dropdown resolutionDropDown;
+    TMP_Dropdown resolutionDropDown;
     List<Resolution> resolutions = new List<Resolution>();
+    int resolutionNum;
+
+    public GameObject UIDataO;
+    UIData uiData;
     // Start is called before the first frame update
     void Start()
     {
-        resolutionDropDown = resolutionDropDownObject.GetComponent<Dropdown>();
+        uiData = UIDataO.GetComponent<UIData>();
+        resolutionDropDown = resolutionDropDownObject.GetComponent<TMP_Dropdown>();
         InitUI();
     }
 
@@ -22,17 +29,38 @@ public class ScreenResolution : MonoBehaviour
     }
     void InitUI()
     {
-        resolutions.AddRange(Screen.resolutions);
+        for(int i=0; i< Screen.resolutions.Length; i++)
+        {
+            if (Screen.resolutions[i].refreshRate == 60)
+            {
+                resolutions.Add(Screen.resolutions[i]);
+            }
+        }
         resolutionDropDown.options.Clear();
 
+        int optionNum = 0;
         foreach(Resolution item in resolutions)
         {
-            Dropdown.OptionData option = new Dropdown.OptionData();
+            TMP_Dropdown.OptionData option = new TMP_Dropdown.OptionData();
             option.text = item.width + " X "  + item.height;
             resolutionDropDown.options.Add(option);
+             
+            if (item.width == Screen.width && item.height == Screen.height)
+                resolutionDropDown.value = optionNum;
+            optionNum++;
         }
-
         resolutionDropDown.RefreshShownValue();
     }
- 
+
+    public void DropboxOptionChange(int x)
+    {
+        resolutionNum = x;
+        Screen.SetResolution(resolutions[resolutionNum].width, resolutions[resolutionNum].height, uiData.screenMode);
+    }
+
+    public void FullScreenBtn(bool isFull)
+    {
+        screenMode = isFull ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed;
+    }
+
 }
--- Assets/Scripts/UI/UIData.cs
+++ Assets/Scripts/UI/UIData.cs
@@ -6,11 +6,13 @@ public class UIData : MonoBehaviour
 {
     public bool isStart;
     public bool isSoundOn;
+    public FullScreenMode screenMode;
     // Start is called before the first frame update
     void Start()
     {
         isStart = false;
         isSoundOn = true;
+        screenMode = FullScreenMode.FullScreenWindow;
     }
 
     // Update is called once per frame
