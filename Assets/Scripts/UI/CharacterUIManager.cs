338b5e91af755ab62b51a88cf76d6ea249417286
agfalcon <agfalcon12@gmail.com>
2022-11-30 17:43:59 +0900

    mapmap

--- Assets/Scripts/UI/CharacterUIManager.cs
+++ Assets/Scripts/UI/CharacterUIManager.cs
@@ -7,22 +7,30 @@ public class CharacterUIManager : MonoBehaviour
 {
     public GameObject escUI;
     public bool isESC = false;
+    public bool isM = false;
     float timer;
+    float timer2;
     float waitingTime;
     bool isWait = false;
+    bool isWait2 = false;
     public GameObject MainCharacter;
     public GameObject MainCam;
+    public GameObject MiniCam;
+    Camera camera;
     // Start is called before the first frame update
     void Start()
     {
+        timer2 = 0.0f;
         timer = 0.0f;
         waitingTime = 0.2f;
+        camera = MiniCam.GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
+        timer2 += Time.deltaTime;
 
         if (timer > waitingTime)
         {
@@ -30,7 +38,13 @@ public class CharacterUIManager : MonoBehaviour
             timer = 0;
             isWait = false;
         }
+        if (timer2 > waitingTime)
+        {
+            timer2 = 0;
+            isWait2 = false;
+        }
         isEsc();
+        isMap();
     }
 
     public void isEsc()
@@ -43,14 +57,34 @@ public class CharacterUIManager : MonoBehaviour
             timer = 0.0f;
             MainCam.GetComponent<CamRotate>().enabled = true;
         }
-        else if(!isWait && !isESC && Input.GetKey(KeyCode.Escape))
+        else if (!isWait && !isESC && Input.GetKey(KeyCode.Escape))
         {
             isESC = true;
             escUI.SetActive(true);
             isWait = true;
             timer = 0.0f;
             MainCam.GetComponent<CamRotate>().enabled = false;
-            
+
+        }
+    }
+    public void isMap()
+    {
+        if (!isWait2 && isM && Input.GetKey(KeyCode.M))
+        {
+            camera.enabled = false;
+            isM = false;
+            isWait2 = true;
+            timer2 = 0.0f;
+            MainCam.GetComponent<CamRotate>().enabled = true;
+        }
+        else if (!isWait2 && !isM && Input.GetKey(KeyCode.M))
+        {
+            camera.enabled = true;
+            isM = true;
+            isWait2 = true;
+            timer2 = 0.0f;
+            MainCam.GetComponent<CamRotate>().enabled = false;
+
         }
     }
 
