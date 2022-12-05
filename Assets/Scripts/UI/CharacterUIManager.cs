52399ade81e18aa0161a24e39d2bfbc9d1f7c021
agfalcon <agfalcon12@gmail.com>
2022-12-05 20:04:26 +0900

    good!

--- Assets/Scripts/UI/CharacterUIManager.cs
+++ Assets/Scripts/UI/CharacterUIManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
