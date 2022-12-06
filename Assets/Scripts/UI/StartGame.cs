4de0898dcf01871ca707c624829708c215e062f1
sgJung <alow9942@daum.net>
2022-12-06 18:39:41 +0900

    gfd

--- Assets/Scripts/UI/StartGame.cs
+++ Assets/Scripts/UI/StartGame.cs
@@ -7,6 +7,7 @@ public class StartGame : MonoBehaviour
 {
     public GameObject obj1;
     public GameObject UIDataO;
+    public GameObject PlayerUI;
     public GameObject MainCharacter;
     UIData uiData;
     // Start is called before the first frame update
@@ -16,6 +17,9 @@ public class StartGame : MonoBehaviour
         uiData = UIDataO.GetComponent<UIData>();
         uiData.isStart = true;
         MainCharacter.SetActive(true);
+        PlayerUI.SetActive(true);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
 
         Invoke(nameof(CameraRotateEnabled), 1f);
     }
